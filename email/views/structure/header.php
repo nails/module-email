16f8c2db53d2811dbed4d7494f694e43d4c1c147
Pablo de la Peña <p@shedcollective.org>
2015-06-12 08:16:49 +0100

    Small changes to the default email header and footer

--- email/views/structure/header.php
+++ email/views/structure/header.php
@@ -1,315 +1,315 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title><?=$email_subject?></title>
-		<meta charset="utf-8">
-		<style type="text/css">
+    <head>
+        <title><?=$email_subject?></title>
+        <meta charset="utf-8">
+        <style type="text/css">
 
-			body,
-			#BodyImposter
-			{
-				margin:0;
-				padding:0;
-				font-size:13px;
-				font-family:"HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
-				line-height:1.75em;
-				max-width:600px;
-				margin:auto;
-				color:#333;
-			}
+            body,
+            #BodyImposter
+            {
+                margin:0;
+                padding:0;
+                font-size:13px;
+                font-family:"HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+                line-height:1.75em;
+                max-width:600px;
+                margin:auto;
+                color:#333;
+            }
 
-			.padder
-			{
-				padding: 20px;
-			}
+            .padder
+            {
+                padding: 20px;
+            }
 
-			h1
-			{
-				line-height:1.5em;
-				font-style:italic;
-				font-weight:bold;
-			}
+            h1
+            {
+                line-height:1.5em;
+                font-style:italic;
+                font-weight:bold;
+            }
 
-			h2
-			{
-				font-size:1.2em;
-				font-weight:bold;
-				margin-top:3em;
-				border-bottom:1px solid #ECECEC;
-				padding-bottom:0.5em;
-			}
+            h2
+            {
+                font-size:1.2em;
+                font-weight:bold;
+                margin-top:3em;
+                border-bottom:1px solid #ECECEC;
+                padding-bottom:0.5em;
+            }
 
-			h3,h4,h5,h6
-			{
-				font-size:1em;
-				font-weight:bold;
-				margin-bottom:1em;
-			}
+            h3,h4,h5,h6
+            {
+                font-size:1em;
+                font-weight:bold;
+                margin-bottom:1em;
+            }
 
-			p
-			{
-				margin-bottom:1em;
-			}
+            p
+            {
+                margin-bottom:1em;
+            }
 
-			.text-center
-			{
-				text-align: center;
-			}
+            .text-center
+            {
+                text-align: center;
+            }
 
-			.text-left
-			{
-				text-align: left;
-			}
+            .text-left
+            {
+                text-align: left;
+            }
 
-			.text-right
-			{
-				text-align: right;
-			}
+            .text-right
+            {
+                text-align: right;
+            }
 
-			small
-			{
-				font-size:0.8em;
-			}
+            small
+            {
+                font-size:0.8em;
+            }
 
-			blockquote
-			{
-				border-left: 4px solid #EFEFEF;
-				padding-left: 10px;
-				margin-left: 0px;
-				font-style: italic;
-				font-size: 1.3em;
-				font-weight: lighter;
-				color: #777777;
-			}
+            blockquote
+            {
+                border-left: 4px solid #EFEFEF;
+                padding-left: 10px;
+                margin-left: 0px;
+                font-style: italic;
+                font-size: 1.3em;
+                font-weight: lighter;
+                color: #777777;
+            }
 
-			ul
-			{
-				margin:0;
-				margin-bottom:1em;
-				padding:0;
-			}
+            ul
+            {
+                margin:0;
+                margin-bottom:1em;
+                padding:0;
+            }
 
-			ul li
-			{
-				margin:0;
-				padding:0;
-				list-style-type: none;
-			}
+            ul li
+            {
+                margin:0;
+                padding:0;
+                list-style-type: none;
+            }
 
-			hr
-			{
-				border:none;
-				border-top:1px dotted #CCCCCC;
-				margin: 30px 0;
-			}
+            hr
+            {
+                border:none;
+                border-top:1px dotted #CCCCCC;
+                margin: 30px 0;
+            }
 
-	        hr
+            hr
             {
                 border:0;
                 margin:20px 0;
                 border-bottom:1px solid #F1F1F1
             }
 
-			.footer
-			{
-				border-top:1px solid #ECECEC;
-				margin-top:2em;
-			}
+            .footer
+            {
+                border-top:1px solid #ECECEC;
+                margin-top:2em;
+            }
 
-			table.default-style
-			{
-				border:1px solid #CCCCCC;
-				width:100%;
-				border-collapse: collapse;
-			}
+            table.default-style
+            {
+                border:1px solid #CCCCCC;
+                width:100%;
+                border-collapse: collapse;
+            }
 
-			table.default-style th
-			{
-				background:#EFEFEF;
-				border-bottom:1px dotted #CCCCCC;
-				padding: 5px 10px;
-			}
+            table.default-style th
+            {
+                background:#EFEFEF;
+                border-bottom:1px dotted #CCCCCC;
+                padding: 5px 10px;
+            }
 
-			table.default-style td
-			{
-				padding:10px;
-				vertical-align: top;
-			}
+            table.default-style td
+            {
+                padding:10px;
+                vertical-align: top;
+            }
 
-			table.default-style td.left-header-cell
-			{
-				width:125px;
-				font-weight:bold;
-				background:#ECECEC;
-				border-right:1px solid #CCCCCC;
-			}
+            table.default-style td.left-header-cell
+            {
+                width:125px;
+                font-weight:bold;
+                background:#ECECEC;
+                border-right:1px solid #CCCCCC;
+            }
 
-			table.default-style th.center,
-			table.default-style td.center
-			{
-				text-align: center;
-			}
+            table.default-style th.center,
+            table.default-style td.center
+            {
+                text-align: center;
+            }
 
-			table.default-style th.right,
-			table.default-style td.right
-			{
-				text-align: right;
-			}
+            table.default-style th.right,
+            table.default-style td.right
+            {
+                text-align: right;
+            }
 
-			table.default-style tr.line-bottom td
-			{
-				border-bottom:1px dotted #CCCCCC;
-			}
+            table.default-style tr.line-bottom td
+            {
+                border-bottom:1px dotted #CCCCCC;
+            }
 
-			table.default-style td small
-			{
-				display:block;
-			}
+            table.default-style td small
+            {
+                display:block;
+            }
 
-			img.thumbnail
-			{
-				padding:6px;
-				border:1px solid #CCCCCC;
-				background:#F9F9F9;
-				-moz-border-radius: 2px;
-				-webkit-border-radius: 2px;
-				border-radius: 2px;
+            img.thumbnail
+            {
+                padding:6px;
+                border:1px solid #CCCCCC;
+                background:#F9F9F9;
+                -moz-border-radius: 2px;
+                -webkit-border-radius: 2px;
+                border-radius: 2px;
 
-				-moz-box-shadow: 0px 1px 1px #888888;
-				-webkit-box-shadow: 0px 1px 1px #888888;
-				box-shadow: 0px 1px 1px #888888;
-			}
+                -moz-box-shadow: 0px 1px 1px #888888;
+                -webkit-box-shadow: 0px 1px 1px #888888;
+                box-shadow: 0px 1px 1px #888888;
+            }
 
-			.heads-up
-			{
-				padding:10px;
-				border:1px solid #CCCCCC;
-				background:#EFEFEF;
-				-webkit-border-radius:3px;
-				-moz-border-radius:3px;
-				-o-border-radius:3px;
-				border-radius:3px;
+            .heads-up
+            {
+                padding:10px;
+                border:1px solid #CCCCCC;
+                background:#EFEFEF;
+                -webkit-border-radius:3px;
+                -moz-border-radius:3px;
+                -o-border-radius:3px;
+                border-radius:3px;
 
-				-moz-box-shadow: 0px 1px 1px #CCCCCC;
-				-webkit-box-shadow: 0px 1px 1px #CCCCCC;
-				box-shadow: 0px 1px 1px #CCCCCC;
-			}
+                -moz-box-shadow: 0px 1px 1px #CCCCCC;
+                -webkit-box-shadow: 0px 1px 1px #CCCCCC;
+                box-shadow: 0px 1px 1px #CCCCCC;
+            }
 
-			.heads-up.warning,
-			.heads-up.warning hr
-			{
-				background-color: #fcf8e3;
-				border-color: #faebcc;
-				color: #8a6d3b;
-				margin: 1em 0;
-			}
+            .heads-up.warning,
+            .heads-up.warning hr
+            {
+                background-color: #fcf8e3;
+                border-color: #faebcc;
+                color: #8a6d3b;
+                margin: 1em 0;
+            }
 
-			a.button,
-			a.btn
-			{
-				background: #eee; /* Old browsers */
-				background: #eee -moz-linear-gradient(top, rgba(255,255,255,.2) 0%, rgba(0,0,0,.2) 100%); /* FF3.6+ */
-				background: #eee -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,255,255,.2)), color-stop(100%,rgba(0,0,0,.2))); /* Chrome,Safari4+ */
-				background: #eee -webkit-linear-gradient(top, rgba(255,255,255,.2) 0%,rgba(0,0,0,.2) 100%); /* Chrome10+,Safari5.1+ */
-				background: #eee -o-linear-gradient(top, rgba(255,255,255,.2) 0%,rgba(0,0,0,.2) 100%); /* Opera11.10+ */
-				background: #eee -ms-linear-gradient(top, rgba(255,255,255,.2) 0%,rgba(0,0,0,.2) 100%); /* IE10+ */
-				background: #eee linear-gradient(top, rgba(255,255,255,.2) 0%,rgba(0,0,0,.2) 100%); /* W3C */
-				border: 1px solid #AAAAAA;
-				border-top: 1px solid #CCCCCC;
-				border-left: 1px solid #CCCCCC;
-				-moz-border-radius: 3px;
-				-webkit-border-radius: 3px;
-				border-radius: 3px;
-				color: #444444;
-				display: inline-block;
-				font-weight: bold;
-				text-decoration: none;
-				text-shadow: 0 1px rgba(255, 255, 255, .75);
-				cursor: pointer;
-				padding: 8px 10px;
-				font-family: "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif;
-			}
+            a.button,
+            a.btn
+            {
+                background: #eee; /* Old browsers */
+                background: #eee -moz-linear-gradient(top, rgba(255,255,255,.2) 0%, rgba(0,0,0,.2) 100%); /* FF3.6+ */
+                background: #eee -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,255,255,.2)), color-stop(100%,rgba(0,0,0,.2))); /* Chrome,Safari4+ */
+                background: #eee -webkit-linear-gradient(top, rgba(255,255,255,.2) 0%,rgba(0,0,0,.2) 100%); /* Chrome10+,Safari5.1+ */
+                background: #eee -o-linear-gradient(top, rgba(255,255,255,.2) 0%,rgba(0,0,0,.2) 100%); /* Opera11.10+ */
+                background: #eee -ms-linear-gradient(top, rgba(255,255,255,.2) 0%,rgba(0,0,0,.2) 100%); /* IE10+ */
+                background: #eee linear-gradient(top, rgba(255,255,255,.2) 0%,rgba(0,0,0,.2) 100%); /* W3C */
+                border: 1px solid #AAAAAA;
+                border-top: 1px solid #CCCCCC;
+                border-left: 1px solid #CCCCCC;
+                -moz-border-radius: 3px;
+                -webkit-border-radius: 3px;
+                border-radius: 3px;
+                color: #444444;
+                display: inline-block;
+                font-weight: bold;
+                text-decoration: none;
+                text-shadow: 0 1px rgba(255, 255, 255, .75);
+                cursor: pointer;
+                padding: 8px 10px;
+                font-family: "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif;
+            }
 
-			a.button.large,
-			a.btn.large
-			{
-				font-size:1em;
-				padding: 10px 15px;
-			}
+            a.button.large,
+            a.btn.large
+            {
+                font-size:1em;
+                padding: 10px 15px;
+            }
 
-			a.button.small,
-			a.btn.small
-			{
-				font-size:0.8em;
-				padding: 2px 7px;
-			}
+            a.button.small,
+            a.btn.small
+            {
+                font-size:0.8em;
+                padding: 2px 7px;
+            }
 
-			a.button:hover,
-			a.btn:hover
-			{
-				color: #222222;
-				background: #DDDDDD; /* Old browsers */
-				background: #DDDDDD -moz-linear-gradient(top, rgba(255,255,255,.3) 0%, rgba(0,0,0,.3) 100%); /* FF3.6+ */
-				background: #DDDDDD -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,255,255,.3)), color-stop(100%,rgba(0,0,0,.3))); /* Chrome,Safari4+ */
-				background: #DDDDDD -webkit-linear-gradient(top, rgba(255,255,255,.3) 0%,rgba(0,0,0,.3) 100%); /* Chrome10+,Safari5.1+ */
-				background: #DDDDDD -o-linear-gradient(top, rgba(255,255,255,.3) 0%,rgba(0,0,0,.3) 100%); /* Opera11.10+ */
-				background: #DDDDDD -ms-linear-gradient(top, rgba(255,255,255,.3) 0%,rgba(0,0,0,.3) 100%); /* IE10+ */
-				background: #DDDDDD linear-gradient(top, rgba(255,255,255,.3) 0%,rgba(0,0,0,.3) 100%); /* W3C */
-				border: 1px solid #888888;
-				border-top: 1px solid #AAAAAA;
-				border-left: 1px solid #AAAAAA; }
+            a.button:hover,
+            a.btn:hover
+            {
+                color: #222222;
+                background: #DDDDDD; /* Old browsers */
+                background: #DDDDDD -moz-linear-gradient(top, rgba(255,255,255,.3) 0%, rgba(0,0,0,.3) 100%); /* FF3.6+ */
+                background: #DDDDDD -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,255,255,.3)), color-stop(100%,rgba(0,0,0,.3))); /* Chrome,Safari4+ */
+                background: #DDDDDD -webkit-linear-gradient(top, rgba(255,255,255,.3) 0%,rgba(0,0,0,.3) 100%); /* Chrome10+,Safari5.1+ */
+                background: #DDDDDD -o-linear-gradient(top, rgba(255,255,255,.3) 0%,rgba(0,0,0,.3) 100%); /* Opera11.10+ */
+                background: #DDDDDD -ms-linear-gradient(top, rgba(255,255,255,.3) 0%,rgba(0,0,0,.3) 100%); /* IE10+ */
+                background: #DDDDDD linear-gradient(top, rgba(255,255,255,.3) 0%,rgba(0,0,0,.3) 100%); /* W3C */
+                border: 1px solid #888888;
+                border-top: 1px solid #AAAAAA;
+                border-left: 1px solid #AAAAAA; }
 
-				.button:active,
-				button:active,
-				input[type="submit"]:active,
-				input[type="reset"]:active,
-				input[type="button"]:active {
-				border: 1px solid #666666;
-				background: #CCCCCC; /* Old browsers */
-				background: #CCCCCC -moz-linear-gradient(top, rgba(255,255,255,.35) 0%, rgba(10,10,10,.4) 100%); /* FF3.6+ */
-				background: #CCCCCC -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,255,255,.35)), color-stop(100%,rgba(10,10,10,.4))); /* Chrome,Safari4+ */
-				background: #CCCCCC -webkit-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* Chrome10+,Safari5.1+ */
-				background: #CCCCCC -o-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* Opera11.10+ */
-				background: #CCCCCC -ms-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* IE10+ */
-				background: #CCCCCC linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* W3C */
-			}
+                .button:active,
+                button:active,
+                input[type="submit"]:active,
+                input[type="reset"]:active,
+                input[type="button"]:active {
+                border: 1px solid #666666;
+                background: #CCCCCC; /* Old browsers */
+                background: #CCCCCC -moz-linear-gradient(top, rgba(255,255,255,.35) 0%, rgba(10,10,10,.4) 100%); /* FF3.6+ */
+                background: #CCCCCC -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,255,255,.35)), color-stop(100%,rgba(10,10,10,.4))); /* Chrome,Safari4+ */
+                background: #CCCCCC -webkit-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* Chrome10+,Safari5.1+ */
+                background: #CCCCCC -o-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* Opera11.10+ */
+                background: #CCCCCC -ms-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* IE10+ */
+                background: #CCCCCC linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* W3C */
+            }
 
-			a.button:active,
-			a.btn:active
-			{
-				border: 1px solid #666666;
-				background: #CCCCCC; /* Old browsers */
-				background: #CCCCCC -moz-linear-gradient(top, rgba(255,255,255,.35) 0%, rgba(10,10,10,.4) 100%); /* FF3.6+ */
-				background: #CCCCCC -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,255,255,.35)), color-stop(100%,rgba(10,10,10,.4))); /* Chrome,Safari4+ */
-				background: #CCCCCC -webkit-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* Chrome10+,Safari5.1+ */
-				background: #CCCCCC -o-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* Opera11.10+ */
-				background: #CCCCCC -ms-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* IE10+ */
-				background: #CCCCCC linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* W3C */ }
+            a.button:active,
+            a.btn:active
+            {
+                border: 1px solid #666666;
+                background: #CCCCCC; /* Old browsers */
+                background: #CCCCCC -moz-linear-gradient(top, rgba(255,255,255,.35) 0%, rgba(10,10,10,.4) 100%); /* FF3.6+ */
+                background: #CCCCCC -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,255,255,.35)), color-stop(100%,rgba(10,10,10,.4))); /* Chrome,Safari4+ */
+                background: #CCCCCC -webkit-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* Chrome10+,Safari5.1+ */
+                background: #CCCCCC -o-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* Opera11.10+ */
+                background: #CCCCCC -ms-linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* IE10+ */
+                background: #CCCCCC linear-gradient(top, rgba(255,255,255,.35) 0%,rgba(10,10,10,.4) 100%); /* W3C */ }
 
-				.button.full-width,
-				button.full-width,
-				input[type="submit"].full-width,
-				input[type="reset"].full-width,
-				input[type="button"].full-width {
-				width: 100%;
-				padding-left: 0 !important;
-				padding-right: 0 !important;
-				text-align: center;
-			}
+                .button.full-width,
+                button.full-width,
+                input[type="submit"].full-width,
+                input[type="reset"].full-width,
+                input[type="button"].full-width {
+                width: 100%;
+                padding-left: 0 !important;
+                padding-right: 0 !important;
+                text-align: center;
+            }
 
-		</style>
-	</head>
-	<body>
-	<div id="BodyImposter">
-	<div class="padder">
-	<?php
+        </style>
+    </head>
+    <body>
+    <div id="BodyImposter">
+    <div class="padder">
+    <?php
 
-		echo '<h1>';
-			echo $email_subject;
-			echo '<hr />';
-		echo '</h1>';
+        echo '<h1>';
+            echo $email_subject;
+            echo '<hr />';
+        echo '</h1>';
 
-		if (isset($sent_to->first) && $sent_to->first) {
+        if (isset($sent_to->first) && $sent_to->first && empty($email_no_greeting)) {
 
-			echo '<p>Hi ' . $sent_to->first . ',</p>';
-		}
+            echo '<p>Hi ' . $sent_to->first . ',</p>';
+        }
--- email/views/structure/header_plaintext.php
+++ email/views/structure/header_plaintext.php
@@ -4,8 +4,8 @@
 
 <?php
 
-	if (isset($sent_to->first) && $sent_to->first) {
+if (isset($sent_to->first) && $sent_to->first && empty($email_no_greeting)) {
 
-		echo 'Hi ' . $sent_to->first . ',' . "\n\n";
+    echo 'Hi ' . $sent_to->first . ',' . "\n\n";
 
-	}
+}
