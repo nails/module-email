5fcae1f6eb8e0cd52a1b9e4ccac396a2b40c8e8b
Pablo de la Peña <p@shedcollective.org>
2018-07-20 13:32:06 +0100

    Increasing size of read and link click counters

---
+++ migrations/6.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Migration:   6
+ * Started:     20/07/2017
+ * Finalised:   20/07/2017
+ *
+ * @package     Nails
+ * @subpackage  module-email
+ * @category    Database Migration
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Database\Migration\Nailsapp\ModuleEmail;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration6 extends Base
+{
+    /**
+     * Execute the migration
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}email_archive` CHANGE `read_count` `read_count` INT(11) UNSIGNED NOT NULL DEFAULT 0;');
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}email_archive` CHANGE `link_click_count` `link_click_count` INT(11) UNSIGNED NOT NULL DEFAULT 0;');
+    }
+}
