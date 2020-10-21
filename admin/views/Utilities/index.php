488492e51068365f667a8cd985ca3a34e764e07d
Pablo de la Peña <p@shedcollective.org>
2020-10-21 16:36:55 +0100

    Adds email configurationd etails to email test page

--- admin/views/Utilities/index.php
+++ admin/views/Utilities/index.php
@@ -21,4 +21,69 @@
         <?=form_submit('submit', 'Send Test Email', 'class="btn btn-primary"')?>
     </p>
     <?=form_close()?>
+    <?php
+    if (isSuperuser()) {
+
+        ?>
+        <hr>
+        <h2>
+            Email configuration
+        </h2>
+        <fieldset>
+            <legend>SMTP</legend>
+            <?php
+            echo form_field([
+                'key'      => '',
+                'label'    => 'Host',
+                'default'  => \Nails\Config::get('EMAIL_HOST'),
+                'readonly' => true,
+            ]);
+
+            echo form_field([
+                'key'      => '',
+                'label'    => 'User',
+                'default'  => \Nails\Config::get('EMAIL_USERNAME'),
+                'readonly' => true,
+            ]);
+
+            echo form_field([
+                'key'      => '',
+                'label'    => 'Password',
+                'default'  => mask(\Nails\Config::get('EMAIL_PASSWORD')),
+                'readonly' => true,
+            ]);
+
+            echo form_field([
+                'key'      => '',
+                'label'    => 'Port',
+                'default'  => \Nails\Config::get('EMAIL_PORT'),
+                'readonly' => true,
+            ]);
+            ?>
+        </fieldset>
+        <fieldset>
+            <legend>Overrides and Whitelist</legend>
+            <?php
+
+            echo form_field([
+                'key'      => '',
+                'label'    => 'To Override',
+                'default'  => \Nails\Config::get('EMAIL_OVERRIDE') ?: \Nails\Config::get('APP_DEVELOPER_EMAIL'),
+                'info'     => 'If defined, all email is routed to this address',
+                'readonly' => true,
+            ]);
+
+            echo form_field_textarea([
+                'key'      => '',
+                'label'    => 'Whitelist',
+                'default'  => \Nails\Config::get('EMAIL_WHITELIST'),
+                'info'     => 'If defined, email is only released if the "to" address is whitelisted',
+                'readonly' => true,
+            ]);
+            ?>
+        </fieldset>
+        <?php
+    }
+
+    ?>
 </div>
